4b7a07e2ea0cd4231928b79b2fbeb5be9beab0e1
lucymait <lucy.maitland@hotmail.com>
2020-04-17 16:42:42 +0100

    changes

--- p4/settings.py
+++ p4/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jwt_auth',
-    'fitness',
     'rest_framework',
+    'fitness',
+    'jwt_auth',
     'frontend'
 ]
 
