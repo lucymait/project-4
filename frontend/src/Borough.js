dc984181d1694297ed18f4276dcf3a76524966c7
lucymait <lucy.maitland@hotmail.com>
2020-04-18 14:08:19 +0100

    changes

--- frontend/src/Borough.js
+++ frontend/src/Borough.js
@@ -1,41 +1,41 @@
-// import React, { useState, useEffect } from 'react'
-// import ReactDOM from 'react-dom'
-// import 'bulma'
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import 'bulma'
 
-// const Borough = () => {
-//   const [data, setData] = useState([])
+const Borough = () => {
+  const [data, setData] = useState([])
 
-//   useEffect(() => {
-//     fetch('/api/fitness/borough')
-//       .then(resp => resp.json())
-//       .then(resp => setData(resp))
-//   }, [])
+  useEffect(() => {
+    fetch('/api/fitness/borough')
+      .then(resp => resp.json())
+      .then(resp => setData(resp))
+  }, [])
 
-//   return <>
-//     <section className="section">
-//       <h1 className="title">Pick a Borough</h1>
-//       <div className="container">
-//         <div className="columns is-multiline">
-//           {data.map((borough) => {
-//             return <div className="column is-one-third" key={borough.id}>
-//               <div className="card">
-//                 <div className="card-content">
-//                   <h2 className="subtitle">{borough.name}</h2>
-//                 </div>
-//                 <div className="card-image">
-//                   <figure className="card-image is-3by3">
-//                     <img src={borough.image} className="borough-image"></img>
-//                   </figure>
-//                 </div>
-//               </div>
-//             </div>
-//           })}
-//         </div>
-//       </div>
-//     </section>
-//   </>
+  return <>
+    <section className="section">
+      <h1 className="title">Pick a Borough</h1>
+      <div className="container">
+        <div className="columns is-multiline">
+          {data.map((borough) => {
+            return <div className="column is-one-third" key={borough.id}>
+              <div className="card">
+                <div className="card-content">
+                  <h2 className="subtitle">{borough.name}</h2>
+                </div>
+                <div className="card-image">
+                  <figure className="card-image is-3by3">
+                    <img src={borough.image} className="borough-image"></img>
+                  </figure>
+                </div>
+              </div>
+            </div>
+          })}
+        </div>
+      </div>
+    </section>
+  </>
 
 
-// }
+}
 
-// export default Borough
+export default Borough
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,44 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bulma'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import './style.scss'
+import Borough from './Borough'
+import Register from './Register'
 
 const App = () => {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    fetch('/api/fitness/borough')
-      .then(resp => resp.json())
-      .then(resp => setData(resp))
-  }, [])
-
-
-  return <>
-    <section className="section">
-      <h1 className="title">Pick a Borough</h1>
-      <div className="container">
-        <div className="columns is-multiline">
-          {data.map((borough) => {
-            return <div className="column is-one-third" key={borough.id}>
-              <div className="card">
-                <div className="card-content">
-                  <h2 className="subtitle">{borough.name}</h2>
-                </div>
-                <div className="card-image">
-                  <figure className="card-image is-3by3">
-                    <img src={borough.image} className="borough-image"></img>
-                  </figure>
-                </div>
-              </div>
-            </div>
-          })}
-        </div>
-      </div>
-    </section>
-  </>
-
-
+  return <HashRouter>
+    {/* <Navbar /> */}
+    <Switch>
+      <Route exact path = {'/borough'} component={Borough}/>
+      <Route path="/register" component={Register}/>
+    </Switch>
+  </HashRouter>
 }
+
 
 ReactDOM.render(
   <App />,
