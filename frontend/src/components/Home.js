764c4838f07335e8d9683b2783e76d2ad8549dce
annie-kayal <anamikakayal@hotmail.com>
2020-04-18 17:41:47 +0100

    homepage styled

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import '../../src/style.scss'
+import { Link } from 'react-router-dom'
 
 const Home = () => (
-  <h1>Hello World</h1>
+  <section className="hero is-light is-fullheight homepage">
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <div className="title">
+          <h1>FITBOOK</h1>
+          <img src='https://i.imgur.com/50EzKYk.png'/>
+        </div>
+        <div className="subtitle">
+          <Link to='/register'><button className="button is-large">Register</button></Link>
+          <Link to='/login'><button className="button is-large">Login</button></Link>
+        </div>
+      </div>
+    </div>
+  </section>
+
 )
 
 export default Home
