98e7253d4069e0db208e486513cf0e1180e2540f
annie-kayal <anamikakayal@hotmail.com>
2020-04-21 17:54:22 +0100

    profile nearly styled

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
+
 import NavBar from './NavBar'
 
 import auth from '../../lib/auth'
+import axios from 'axios'
 
 const Profile = () => {
   const [profile, setProfile] = useState({ fitness: [] })
@@ -16,19 +18,36 @@ const Profile = () => {
       })
   }, [])
 
+
+  function deleteFitnessClass(e) {
+    const id = (e.target.parentNode.id)
+    axios.delete(`api/fitness/bookedclass/${id}`, { headers: { Authorization: `Bearer ${auth.getToken()}` } })
+      .then(() => {
+        fetch('api/profile', { headers: { Authorization: `Bearer ${auth.getToken()}` } })
+          .then(resp => resp.json())
+          .then(resp => {
+            setProfile(resp)
+          })
+      })
+  }
+  
   return <>
-    <section className="section">
-      <div className="container has-text-centered">
-        <img className="booking-image" src= { profile.image === null ?  'https://static.thenounproject.com/png/629576-200.png' : `http://localhost:8000${profile.image}`} />
-        <div className="title">
-          <h3>Welcome back {profile.username}</h3>
+    <section className="section profile-section">
+      <div className="container">
+        <div className="profile-header">
+          <h3>Welcome back {profile.username}!</h3>
+          <img src={profile.image === null ? 'https://static.thenounproject.com/png/629576-200.png' : `http://localhost:8000${profile.image}`} />
         </div>
-        <div className="booked-classes">
-          <h2>Booked Classes</h2>
+        <h2>Booked Classes</h2>
+        <div className="booked-classes ">
+          {profile.fitness}
           {profile.fitness.map(bookedclass => {
-            return <div key={bookedclass.id}>
-              <h2>{bookedclass.name}</h2>
-              <p>{bookedclass.time_of_class}</p>
+            return <div className='card' id={bookedclass.id} key={bookedclass.id}>
+              <div className="card-content">
+                <h3>{bookedclass.name}</h3>
+                <p>{bookedclass.time_of_class}</p>
+              </div>
+              <button onClick={(e) => deleteFitnessClass(e)} className="button">Cancel</button>
             </div>
           })}
         </div>
@@ -36,6 +55,8 @@ const Profile = () => {
     </section>
     <NavBar />
   </>
+
+
 
 
 }
